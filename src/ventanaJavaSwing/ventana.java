599a32de74e5600c8d953ec0066d1d78476f1903
jovicast <myriamcastillo630@gmail.com>
2017-11-08 20:59:06 -0300

    ingresar elementos

---
+++ src/ventanaJavaSwing/ventana.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanaJavaSwing;
+//PASO 1 - IMPORTAR LAS LIBRERIAS
+import java.awt.Color;
+import javax.swing.*;//Importamos TODO de swing
+
+
+/**
+ *
+ * @author LAB07
+ */
+//PASO 2 - HEREDAN LA CLASE DESDE  JFRAME
+public class ventana extends JFrame
+{
+ //PASO 3 - DECLARAR ELEMENTOS A UTILIZAR
+    JPanel panel;
+    JLabel label;
+    JTextField text;
+    JButton boton; 
+    
+}
