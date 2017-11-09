be5750c741f946fd6c74273cf6fee4debf2e56ef
jovicast <myriamcastillo630@gmail.com>
2017-11-08 21:37:56 -0300

    importar clase a la ventana

--- src/javaswing/JavaSwing.java
+++ src/javaswing/JavaSwing.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package javaswing;
+//PASO 8 - IMPORTAR LA CLASE CON LA VENTANA
+import ventanaJavaSwing.ventana;//importar paquete.ventana
+import javax.swing.JFrame;
 
 /**
  *
@@ -16,6 +19,14 @@ public class JavaSwing {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //PASO 9 - LLAMADA A LA CLASE JFRAME
+        ventana  ventana = new Window();//Creo un objeto tipo 
+        ventana.setVisible(true);
+        ventana.setBounds(0, 0, 600, 600);
+        //La siguiente inst. le pone titulo a la ventana 
+        ventana.SetTitle("Mi primera interfaz JavaSwing");
+        //La siguiente sentencia es para finalizar el proyecto el cerrea la ventana
+       ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
