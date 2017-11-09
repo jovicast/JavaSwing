473da5ac82f1b863d9b443a04ba05a0a770f365a
jovicast <myriamcastillo630@gmail.com>
2017-11-08 21:19:31 -0300

    inicializacion de objetos

--- src/ventanaJavaSwing/ventana.java
+++ src/ventanaJavaSwing/ventana.java
@@ -6,6 +6,7 @@
 package ventanaJavaSwing;
 //PASO 1 - IMPORTAR LAS LIBRERIAS
 import java.awt.Color;
+import static javafx.scene.input.KeyCode.J;
 import javax.swing.*;//Importamos TODO de swing
 
 
@@ -21,5 +22,18 @@ public class ventana extends JFrame
     JLabel label;
     JTextField text;
     JButton boton; 
-    
+    JCheckBox verificacion;
+    JRadioButton presionar;
+    // PASO 4 - DEFINIR EL CONSTRUCTOR 
+    public ventana()
+    {
+    //PASO 5 - CREAR E INICIALIZAR LOS OBJETOS
+        panel = new JPanel();//Creo un objeto Tipo JPanel
+        label = new JLabel();
+        text = new JTextField(15);//15 es la cantidad de caracteres
+        boton = new JButton();  
+        verificacion = new JCheckBox();
+        presionar = new JRadioButton();
+        
+        
 }
